bf60d4ff68380b4069e3de4dc0d01db1d5d3f900
Chris Wilson <chris+github@qwirx.com>
2013-11-05 22:40:08 +0000

    Allow callers to expect a dialog without specifying the response.
    Callers can then check the dialog and send a response themselves, which
    doesn't have to happen in nested event handlers and is therefore easier
    to understand.

--- Dialog.js
+++ Dialog.js
@@ -21,36 +21,66 @@ goog.require('goog.ui.Dialog');
 com.qwirx.ui.Dialog = function(opt_class, opt_useIframeMask, opt_domHelper)
 {
 	goog.base(this, opt_class, opt_useIframeMask, opt_domHelper);
+	this.getHandler().listen(this, goog.ui.Dialog.EventType.AFTER_HIDE,
+		this.clearCurrentDialog, false, this);
+	// Our dialogs default to dispose on hide, to save resources.
+	this.setDisposeOnHide(true);
 };
 
 goog.inherits(com.qwirx.ui.Dialog, goog.ui.Dialog);
 
 com.qwirx.ui.Dialog.isTestMode = false;
+com.qwirx.ui.Dialog.currentDialog = undefined;
 com.qwirx.ui.Dialog.dialogResponse = undefined;
+com.qwirx.ui.Dialog.EventTarget = new goog.events.EventTarget();
 
 com.qwirx.ui.Dialog.prototype.onShow = function()
 {
 	if (com.qwirx.ui.Dialog.isTestMode)
 	{
-		if (com.qwirx.ui.Dialog.dialogResponse)
+		// Allow tests that don't have access to the Dialog object anywhere
+		// to listen to events on this singleton instead, to get access to it.
+		goog.events.dispatchEvent(com.qwirx.ui.Dialog.EventTarget,
+			new goog.events.Event(goog.ui.Dialog.EventType.AFTER_SHOW, this));
+		
+		if (com.qwirx.ui.Dialog.dialogResponse !== undefined)
 		{
+			com.qwirx.ui.Dialog.currentDialog = this;
+			
 			goog.base(this, 'onShow');
 			assertTrue("Dialog should still be visible", this.isVisible());
 			
-			// click a button in the dialog, check that it goes away
-			var buttons = this.getButtonSet();
-			// can't use FakeClickEvent because they're not goog.ui.Buttons,
-			// so they don't convert mousedown+mouseup into click events
-			var button = buttons.getButton(com.qwirx.ui.Dialog.dialogResponse);
-			assertNotNull("No such button " + com.qwirx.ui.Dialog.dialogResponse,
-				button);
-			com.qwirx.test.FakeBrowserEvent.send(goog.events.EventType.CLICK,
-				button);
-			assertFalse("Dialog should have been closed by clicking " +
-				"a button", this.isVisible());
-			
-			com.qwirx.ui.Dialog.dialogResponse = undefined;
-			return true;
+			if (com.qwirx.ui.Dialog.dialogResponse === null)
+			{
+				// don't press any buttons, just leave the dialog open
+			}
+			else
+			{
+				// click a button in the dialog, check that it goes away
+				var buttons = this.getButtonSet();
+				// can't use FakeClickEvent because they're not goog.ui.Buttons,
+				// so they don't convert mousedown+mouseup into click events
+				var button = buttons.getButton(com.qwirx.ui.Dialog.dialogResponse);
+				assertNotNull("No such button " + com.qwirx.ui.Dialog.dialogResponse,
+					button);
+				
+				try
+				{
+					com.qwirx.test.FakeBrowserEvent.send(goog.events.EventType.CLICK,
+						button);
+				}
+				catch (exception)
+				{
+					this.setVisible(false);
+					exception.message += " (the open dialog was closed)";
+					throw exception;
+				}
+				
+				assertFalse("Dialog should have been closed by clicking " +
+					"a button", this.isVisible());
+				
+				return true;
+			}
 		}
 		else
 		{
@@ -65,3 +95,8 @@ com.qwirx.ui.Dialog.prototype.onShow = function()
 	}
 };
 
+com.qwirx.ui.Dialog.prototype.clearCurrentDialog = function(event)
+{
+	com.qwirx.ui.Dialog.currentDialog = undefined;
+	com.qwirx.ui.Dialog.dialogResponse = undefined;
+};
